e7a906fd0cd42b3df6e7ea1c3f24801be4d05a05
Mnkith <munkithabid@gmail.com>
2021-08-25 15:19:03 -0500

    create #assign_fake_transactions callback

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,24 @@
 class Account < ApplicationRecord
   belongs_to :user
   has_many :transactions
+  after_create :assign_fake_transactions
+
+  def assign_fake_transactions 
+    fake_transactions = rand 3..7
+    transaction_types = ['Gas', 'Grossery', 'E service', 'Restaurant', 'Utility']
+
+    fake_transactions.times do
+      transaction_date = Date.today - rand(30)
+      description = transaction_types[ rand 0..4 ]
+      amount = rand(100..1200).to_f / 100 
+      # byebug
+      Transaction.create user_id: self.user_id,
+                         account_id: self.id, 
+                         transaction_date: transaction_date,
+                         description: description,
+                         amount: amount
+
+
+    end
+  end
 end
--- db/migrate/20210813180318_create_transactions.rb
+++ db/migrate/20210813180318_create_transactions.rb
@@ -7,7 +7,7 @@ class CreateTransactions < ActiveRecord::Migration[6.1]
       t.decimal :amount, precision: 10, scale: 2
       t.text :description
 
-      t.timestamps
+      # t.timestamps
     end
   end
 end
